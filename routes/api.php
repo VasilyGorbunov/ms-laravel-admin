d0e7ebdb3779bb868207a4ffe67cc650bed6b91e
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-03-26 16:13:12 +0300

    014 Roles

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,7 +21,12 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
 Route::group(['middleware' => 'auth:api'], function() {
-    Route::apiResource('users', \App\Http\Controllers\UserController::class);
+    Route::get('user', [UserController::class, 'user']);
+    Route::put('users/info', [UserController::class, 'updateInfo']);
+    Route::put('users/password', [UserController::class, 'updatePassword']);
+    
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('roles', RoleController::class);
 });
 
 
